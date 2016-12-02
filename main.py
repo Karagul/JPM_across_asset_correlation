896aa4b678bb44d1d51261f9ee62e495c2cf9b82
fzn0728 <thames.ft@gmail.com>
2016-12-02 14:08:09 -0800

    result

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ Created on Fri Dec  2 09:41:37 2016
 
 from datetime import datetime
 import pandas_datareader.data as wb
-import copy
+
 
 stocklist = ['FXF','FXB','FXE','FXA','SPY','EWJ','EWG','IWV','IYY','FEZ','ONEQ','GLD','SLV','OIL','UNL','JJC','WEAT','CORN','TLT','IEF']
 
 start = datetime(2011,9,19)
 end = datetime(2016,12,1)
 p = wb.DataReader(stocklist,'yahoo',start,end)
-return_ticker_df = p['Adj Close']
-return_df = copy.copy(return_ticker_df)
+return_df = p['Adj Close']
 return_df.columns = ['USD/JPY','GBP/USD','EUR/USD','AUD/USD','S&P','NIKKEI','DAX','RUSSELL','DOW','EURO_50','NASDAQ','GOLD','SILVER','OIL','GAS','COPPER','WEAT','CORN','20yr_Bond','7_10_yr_Bond']
 corr_df = return_df.corr()
+
